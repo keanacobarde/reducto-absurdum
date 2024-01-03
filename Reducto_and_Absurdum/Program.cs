adad2bd3a7dbc11cfd87724b2286eadbe686d6e0
Keana Cobarde <keanacobarde@gmail.com>
2024-01-02 20:08:28 -0600

    Creating a global variables class to hold global functions. Created a centralized Main function within Program class

--- Reducto_and_Absurdum/Program.cs
+++ Reducto_and_Absurdum/Program.cs
@@ -1,49 +1,51 @@
 ﻿using System;
-using DateTime;
-using TimeSpan;
 
-Greeting();
-Menu();
+class Globals {
+    public static void Greeting()
+    {
+        string greeting = @"Welcome to Reducto and Absurdum
+    Your one-stop shop for all your witchcraft and wizardry needs";
+        Console.WriteLine(greeting);
+    }
 
-void Greeting() { 
-string greeting = @"Welcome to Reducto and Absurdum
-Your one-stop shop for all your witchcraft and wizardry needs";
-Console.WriteLine(greeting);
-}
-
-void Menu() {
-    string choice = null;
-    while (choice != "0") {
-        Console.WriteLine(@"Choose an option: 
+    public static void Menu()
+    {
+        string choice = null;
+        while (choice != "0")
+        {
+            Console.WriteLine(@"Choose an option: 
                       0. Exit
                       1. View All Products
                       2. Add a Product
                       3. Delete a Product
                       4. Update a Product");
-        choice = Console.ReadLine();
+            choice = Console.ReadLine();
 
-        switch (choice)
-        {
-            case "0":
-                Console.WriteLine("Goodbye");
-                break;
-            case "1":
-                Console.WriteLine("View All Products");
-                break;
-            case "2":
-                Console.WriteLine("Add a Product");
-                break;
-            case "3":
-                Console.WriteLine("Delete a Product");
-                break;
-            case "4":
-                Console.WriteLine("Update a Product");
-                break;
+            switch (choice)
+            {
+                case "0":
+                    Console.WriteLine("Goodbye");
+                    break;
+                case "1":
+                    Console.WriteLine("View All Products");
+                    break;
+                case "2":
+                    Console.WriteLine("Add a Product");
+                    break;
+                case "3":
+                    Console.WriteLine("Delete a Product");
+                    break;
+                case "4":
+                    Console.WriteLine("Update a Product");
+                    break;
+            }
         }
+
     }
 
-}
 
+
+}
 class Product
 {
     public string Name { get; set; }
@@ -54,13 +56,39 @@ class Product
 
     public int DaysOnShelf
     {
-        get { 
+        get
+        {
             TimeSpan timeOnShelf = DateTime.Now - DateStocked;
             return timeOnShelf.Days;
         }
     }
 
 }
+
+namespace ProductsDB
+{
+
+
+
+
+
+
+}
+
+
+class Program {
+    static void Main(string[] args)
+    {
+        Globals.Greeting();
+        Globals.Menu();
+    }
+
+
+}
+
+
+
+
 
 
 
