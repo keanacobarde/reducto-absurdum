f5b087de54c9edc4411a9ca994af1048f800e2ef
Keana Cobarde <keanacobarde@gmail.com>
2024-01-03 21:19:25 -0600

    Finished searching by category functionality.

--- Reducto_and_Absurdum/CategoryFunctions.cs
+++ Reducto_and_Absurdum/CategoryFunctions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization.Formatters;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,13 +11,20 @@ namespace Reducto_and_Absurdum
     {
         public static void CategoryFind()
         {
+            string choice = null;
             Console.WriteLine("Choose a category");
             for (int i = 0; i < Globals.ProductCategories.Count; i++)
             {
                 Console.WriteLine($"{i + 1} - {Globals.ProductCategories[i].Name}");
             
             }
-            
+            choice = Console.ReadLine();
+            string indxChoice = Globals.ProductCategories[Int32.Parse(choice) - 1].Name;
+            List<Product> searchedProduct = Globals.Products.ToList().Where(cat => cat.ProductTypeId == indxChoice).ToList();
+            foreach (Product prod in searchedProduct)
+            {
+                Console.WriteLine(prod.Name);
+            }
         
         }
     
