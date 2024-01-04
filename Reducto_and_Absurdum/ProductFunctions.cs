c6456978a4345c760b8e4e9719d164da0d790f96
Keana Cobarde <keanacobarde@gmail.com>
2024-01-03 20:08:02 -0600

    Obtained full CRUD, Update is held together by bubblegum and chicken wire and HAS NO INPUT VALIDATION. This will be modified.

--- Reducto_and_Absurdum/ProductFunctions.cs
+++ Reducto_and_Absurdum/ProductFunctions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -41,7 +42,50 @@ namespace Reducto_and_Absurdum
         // UPDATE
         public static void UpdateProducts()
         {
+            string choice = null;
 
+            while (choice != "0")
+            {
+                try
+                {
+                    // loop through products but create a ReadLine
+                    Console.WriteLine("0. Goodbye");
+                    for (int i = 0; i < Globals.Products.Count; i++)
+                    {
+                        Console.WriteLine($"{i + 1}. {Globals.Products[i].Name}");
+
+                    }
+                    Console.WriteLine("Choose the item you wish to edit.");
+                    choice = Console.ReadLine();
+                    object IndxChoice = Globals.Products[Int32.Parse(choice) - 1];
+
+                    // view the properties of the object that you're looking to edit
+                    foreach (PropertyDescriptor desc in TypeDescriptor.GetProperties(IndxChoice))
+                    {
+                        string name = desc.Name;
+                        object value = desc.GetValue(IndxChoice);
+                        Console.WriteLine(name + ": " + value);
+                    }
+                    string propToEdit = Console.ReadLine();
+
+                    Console.WriteLine("What value would you like to change it to?");
+
+                    string valueToChange = Console.ReadLine();
+
+                    foreach (PropertyDescriptor desc in TypeDescriptor.GetProperties(IndxChoice))
+                    {
+                        if (desc.Name == propToEdit) 
+                        {
+                            desc.SetValue(IndxChoice, valueToChange);
+                        }
+                    }
+
+                }
+                catch
+                {
+                    break;
+                }
+            }
         }
         // DELETE 
         public static void DeleteProducts()
